9bdc77a7da41b0ebc7251a43b2a6492a906042c6
omJadhav1 <omjadhav0462@gmail.com>
2023-02-23 00:19:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+const apiurl = 'https://api.github.com/users/'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+form.addEventListener('submit' ,(e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user){
+        getuser(user)
+
+        search.value = ''
+    }
+})
+
+async function getuser(username){
+    try{
+        const {data} = await axios(apiurl + username)
+
+        createUserCard(data)
+        getRepos(username)
+    }catch (error){
+        if(error.response.status == 404){
+            createErrorCard('No profile with this username')
+        }
+    }
+}
+
+function createUserCard(user){
+    const cardHTML = `
+    <div class="card">
+    <div>
+      <a href="${user.html_url}"
+        ><img src="${user.avatar_url}" alt="${user.name}" class="avatar"
+      /></a>
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>
+      ${user.bio}
+      </p>
+
+      <ul>
+        <li>${user.followers}<strong>Followers</strong></li>
+        <li>${user.following}<strong>Following</strong></li>
+        <li>${user.public_repos}<strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos">
+      </div>
+    </div>
+  </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+function createErrorCard(msg){
+    const cardHTML = `
+    <div class="card">
+        <h1>${msg}</h1>
+    </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+async function getRepos(username){
+    try {
+        const {data} = await axios(apiurl + username + '/repos?sort=created') 
+
+        addReposToCard(data)
+    } catch (error) {
+        createErrorCard('Problem fetching repos')
+    }
+}
+
+function addReposToCard(repos){
+    const reposElement = document.getElementById('repos')
+
+    repos
+        .slice(0,5)
+        .forEach(repo => {
+            const repoElement =document.createElement('a')
+            repoElement.classList.add('repo')
+            repoElement.href = repo.html_url
+            repoElement.target = '_blank'
+            repoElement.innerText = repo.name
+
+            reposElement.appendChild(repoElement)
+        })
+}
